83b2b741fd53b6d2dc45f007ee0eb0932bb231a2
Ahmet Gungor <ahmetgungor92@gmail.com>
2021-02-28 17:17:50 +0300

    Added /doc end point.

--- src/adapt_lab_project/frontend/views.py
+++ src/adapt_lab_project/frontend/views.py
@@ -3,8 +3,6 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.shortcuts import render
-
-import markdown
 # Create your views here.
 
 response = {"response": {"test": "value"}}
