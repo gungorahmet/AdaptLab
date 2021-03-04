9932e7b060102b87d83f55d6d14ef9ba21c01d69
Ahmet Gungor <ahmetgungor92@gmail.com>
2021-03-05 01:15:54 +0300

    whitenoise is added to middleware

--- src/adapt_lab_project/adapt_lab_project/settings.py
+++ src/adapt_lab_project/adapt_lab_project/settings.py
@@ -69,6 +69,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -76,7 +77,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
 ROOT_URLCONF = 'adapt_lab_project.urls'
